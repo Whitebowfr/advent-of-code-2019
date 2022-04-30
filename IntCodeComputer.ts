9edebe974db48598c81f28348e13279eb995a7fb
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2022-04-30 14:41:00 +0200

    Added OpCode 9, improved readability

--- IntCodeComputer.ts
+++ IntCodeComputer.ts
@@ -1,5 +1,24 @@
 type IntCode = Array<number>;
 
+const OpCodes = {
+    ADDITION: 1,
+    MULTIPLICATION: 2,
+    INPUT: 3,
+    OUTPUT: 4,
+    JUMP_IF_NOT_ZERO: 5,
+    JUMP_IF_ZERO: 6,
+    JUMP_IF_INFERIOR: 7,
+    JUMP_IF_EQUAL: 8,
+    ADJUST_RELATIVE_BASE: 9,
+    STOP: 99
+}
+
+const ParameterModes = {
+    POSITION: "0",
+    IMMEDIATE: "1",
+    RELATIVE: "2"
+}
+
 export class IntCodeComputer {
     code: IntCode
     inputs: number[]
@@ -7,6 +26,7 @@ export class IntCodeComputer {
     currentStepSize: number;
     currentIndex: number;
     currentInputIndex: number;
+    relativeBase: number;
 
     constructor(codeString: string) {
         this.code = this.parseIntCode(codeString);
@@ -15,6 +35,16 @@ export class IntCodeComputer {
         this.outputQueue = [];
         this.currentStepSize = 4;
         this.inputs = [];
+        this.relativeBase = 0;
+    }
+
+    reset() {
+        this.currentIndex = 0;
+        this.currentInputIndex = 0;
+        this.outputQueue = [];
+        this.currentStepSize = 4;
+        this.inputs = [];
+        this.relativeBase = 0;
     }
 
     setInputs(a: number | number[]) {
@@ -38,103 +68,13 @@ export class IntCodeComputer {
         this.outputQueue = [];
         let stopCode: number = 0;
 
-        while (this.currentIndex < this.code.length && stopCode !== 99) {
+        while (stopCode !== 99) {
             let result = this.step(this.currentIndex)
             stopCode = result.stopCode ?? 0;
             this.currentIndex += this.currentStepSize;
         }
 
         return {code: this.code, outputQueue: this.outputQueue, stopCode: (stopCode === undefined) ? 0 : stopCode}
-            // let stringOpCode = code[i].toString();
-            // if (stringOpCode.length < 2) {
-            //     stringOpCode = "0" + stringOpCode;
-            // }
-            // let parameterModes = stringOpCode.split("")
-
-            // let currentOpCode: number = parseInt(parameterModes.splice(-2, 2).join(""));
-
-            // while (parameterModes.length < 3) {
-            //     parameterModes.unshift("0");
-            // }
-            // parameterModes = parameterModes.reverse();
-            // let pm = parameterModes;
-            // let value: number = 0;
-            // let index: number = 0;
-            // switch (currentOpCode) {
-            //     case 1 :
-            //         value = getValue(pm[0], i + 1) + getValue(pm[1], i + 2);
-            //         if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when adding ${getValue(pm[0], i + 1)} and ${getValue(pm[1], i + 2)} after processing instruction ${code[i]},${code[i+1]},${code[i+2]},${code[i+3]} starting at index ${i}`)
-            //         index = code[i + 3];
-            //         code[index] = value;
-            //         stepSize = 4;
-            //         break;
-            //     case 2 :
-            //         value = getValue(pm[0], i + 1) * getValue(pm[1], i + 2);
-            //         if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when multipliying ${getValue(pm[0], i + 1)} with ${getValue(pm[1], i + 2)} after processing instruction ${code[i]},${code[i+1]},${code[i+2]},${code[i+3]} starting at index ${i} with parameters ${pm}`)
-            //         index = code[i + 3];
-            //         code[index] = value;
-            //         stepSize = 4;
-            //         break;
-            //     case 3 :
-            //         if (Array.isArray(input)) {
-            //             value = input[currentInputIndex]
-            //             currentInputIndex++
-            //         } else {
-            //             value = input;
-            //         }
-            //         index = code[i + 1];
-            //         code[index] = value;
-            //         stepSize = 2;
-            //         break;
-            //     case 4 :
-            //         if (pm[0] === "1") {
-            //             value = code[i + 1]
-            //         } else {
-            //             value = getValue(pm[0], i + 1)
-            //         }
-
-            //         outputQueue.push(value)
-
-            //         if (debug) console.log(`Output : ${value}`);
-            //         stepSize = 2;
-            //         break;
-            //     case 5 :
-            //         if (getValue(pm[0], i + 1) !== 0) {
-            //             i = getValue(pm[1], i + 2);
-            //             stepSize = 0;
-            //         } else {
-            //             stepSize = 3;
-            //         }
-            //         break;
-            //     case 6 :
-            //         if (getValue(pm[0], i + 1) === 0) {
-            //             i = getValue(pm[1], i + 2);
-            //             stepSize = 0;
-            //         } else {
-            //             stepSize = 3;
-            //         }
-            //         break;
-            //     case 7 :
-            //         value = Number(getValue(pm[0], i + 1) < getValue(pm[1], i + 2));
-            //         if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when comparing ${getValue(pm[0], i + 1)} and ${getValue(pm[1], i + 2)} after processing instruction ${code[i]},${code[i+1]},${code[i+2]},${code[i+3]} starting at index ${i} with parameters ${pm}`)
-            //         index = code[i + 3];
-            //         code[index] = value;
-            //         stepSize = 4;
-            //         break;
-            //     case 8 :
-            //         value = Number(getValue(pm[0], i + 1) === getValue(pm[1], i + 2));
-            //         if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when comparing equality between ${getValue(pm[0], i + 1)} and ${getValue(pm[1], i + 2)} after processing instruction ${code[i]},${code[i+1]},${code[i+2]},${code[i+3]} starting at index ${i} with parameters ${pm}`)
-            //         index = code[i + 3];
-            //         code[index] = value;
-            //         stepSize = 4;
-            //         break;
-            //     case 99 :
-            //         return {code, outputQueue, stopCode: 99};
-            //     default :
-            //         throw new Error(`Unknown OpCode found : ${currentOpCode} at position ${i}`)
-            // }
-
-            // if (debug && currentOpCode !== 4 && currentOpCode !== 5 && currentOpCode !== 6) console.log(`Written ${value} to ${index}`)
     }
 
     step(currentInputIndex: number): { stopCode?: number } {
@@ -150,45 +90,46 @@ export class IntCodeComputer {
         while (parameterModes.length < 3) {
             parameterModes.unshift("0");
         }
+
         parameterModes = parameterModes.reverse();
         let pm = parameterModes;
         let value: number = 0;
         let index: number = 0;
 
         switch (currentOpCode) {
-            case 1:
+            case OpCodes.ADDITION:
                 value = this.getValue(pm[0], i + 1) + this.getValue(pm[1], i + 2);
-                if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when adding ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i}`)
-                index = this.code[i + 3];
+                if (isNaN(value) || value === undefined) throw new Error(`Found ${value} value when adding ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i}`)
+                index = this.getIndex(pm[2], i + 3);
                 this.code[index] = value;
                 this.currentStepSize = 4;
                 break;
-            case 2:
+
+            case OpCodes.MULTIPLICATION:
                 value = this.getValue(pm[0], i + 1) * this.getValue(pm[1], i + 2);
-                if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when multipliying ${this.getValue(pm[0], i + 1)} with ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i} with parameters ${pm}`)
-                index = this.code[i + 3];
+                if (isNaN(value) || value === undefined) throw new Error(`Found ${value} value when multipliying ${this.getValue(pm[0], i + 1)} with ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i}`)
+                index = this.getIndex(pm[2], i + 3);
                 this.code[index] = value;
                 this.currentStepSize = 4;
                 break;
-            case 3:
+
+            case OpCodes.INPUT:
                 if (Array.isArray(this.inputs)) {
                     value = this.inputs[this.currentInputIndex]
                     this.currentInputIndex++
                 }
-                index = this.code[i + 1];
+                index = this.getIndex(pm[0], i + 1);
                 this.code[index] = value;
                 this.currentStepSize = 2;
                 break;
-            case 4:
-                if (pm[0] === "1") {
-                    value = this.code[i + 1]
-                } else {
-                    value = this.getValue(pm[0], i + 1)
-                }
+
+            case OpCodes.OUTPUT:
+                value = this.getValue(pm[0], i + 1)
                 this.currentStepSize = 2;
                 this.outputQueue.push(value);
                 break;
-            case 5:
+
+            case OpCodes.JUMP_IF_NOT_ZERO:
                 if (this.getValue(pm[0], i + 1) !== 0) {
                     this.currentIndex = this.getValue(pm[1], i + 2);
                     this.currentStepSize = 0;
@@ -196,7 +137,8 @@ export class IntCodeComputer {
                     this.currentStepSize = 3;
                 }
                 break;
-            case 6:
+
+            case OpCodes.JUMP_IF_ZERO:
                 if (this.getValue(pm[0], i + 1) === 0) {
                     this.currentIndex = this.getValue(pm[1], i + 2);
                     this.currentStepSize = 0;
@@ -204,24 +146,35 @@ export class IntCodeComputer {
                     this.currentStepSize = 3;
                 }
                 break;
-            case 7:
+
+            case OpCodes.JUMP_IF_INFERIOR:
                 value = Number(this.getValue(pm[0], i + 1) < this.getValue(pm[1], i + 2));
-                if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when comparing ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i} with parameters ${pm}`)
-                index = this.code[i + 3];
+                if (isNaN(value) || value === undefined) throw new Error(`Found ${value} value when comparing ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i}`)
+                index = this.getIndex(pm[2], i + 3);
                 this.code[index] = value;
                 this.currentStepSize = 4;
                 break;
-            case 8:
+
+            case OpCodes.JUMP_IF_EQUAL:
                 value = Number(this.getValue(pm[0], i + 1) === this.getValue(pm[1], i + 2));
-                if (isNaN(value) || value == undefined) throw new Error(`Found ${value} value when comparing equality between ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i} with parameters ${pm}`)
-                index = this.code[i + 3];
+                if (isNaN(value) || value === undefined) throw new Error(`Found ${value} value when comparing equality between ${this.getValue(pm[0], i + 1)} and ${this.getValue(pm[1], i + 2)} after processing instruction ${this.code[i]},${this.code[i + 1]},${this.code[i + 2]},${this.code[i + 3]} starting at index ${i}`)
+                index = this.getIndex(pm[2], i + 3);
                 this.code[index] = value;
                 this.currentStepSize = 4;
                 break;
-            case 99:
-                return { stopCode: 99 };
+
+            case OpCodes.ADJUST_RELATIVE_BASE :
+                value = this.getValue(pm[0], i + 1)
+                if (isNaN(value) || value === undefined) throw new Error (`Tried to offset relative base by ${value} after processing instruction ${this.code[i]}, ${this.code[i + 1]} starting at index ${i}`)
+                this.relativeBase += value;
+                this.currentStepSize = 2;
+                break;
+
+            case OpCodes.STOP:
+                return { stopCode: OpCodes.STOP };
+
             default:
-                throw new Error(`Unknown Opthis.code found : ${currentOpCode} at position ${i}`)
+                throw new Error(`Unknown OpCode found : ${currentOpCode} at position ${i}`)
         }
         return {stopCode: 0}
     }
@@ -229,11 +182,38 @@ export class IntCodeComputer {
     /**
      * A simple function to get the value of a parameter (differenciating between immediate and position values)
      */
-    getValue(parameterMode: string, index: number, code?: IntCode): number {
-        if (parameterMode === "1") {
-            return (code === undefined) ? this.code[index] : code[index];
+    getValue(parameterMode: string, index: number): number {
+        if (index >= this.code.length) this.extendCode(index);
+        
+        if (parameterMode === ParameterModes.IMMEDIATE) {
+            return this.code[index]
+
+        } else if (parameterMode === ParameterModes.RELATIVE) {
+            if (this.relativeBase + this.code[index] >= this.code.length) this.extendCode(this.relativeBase + this.code[index]);
+            return this.code[this.relativeBase + this.code[index]]
+
         } else {
-            return (code === undefined) ? this.code[this.code[index]] : code[code[index]];
+            if (this.code[index] >= this.code.length) this.extendCode(this.code[index]);
+            return this.code[this.code[index]]
+
+        }
+    }
+
+    getIndex(parameterMode: string, index: number) : number {
+        if (index >= this.code.length) this.extendCode(index)
+
+        if (parameterMode === ParameterModes.RELATIVE) {
+            if (this.relativeBase + this.code[index] >= this.code.length) this.extendCode(this.relativeBase + this.code[index]);
+            return this.relativeBase + this.code[index]
+
+        } else {
+            return this.code[index]
+        }
+    }
+
+    extendCode(length: number) {
+        while (this.code.length <= length) {
+            this.code.push(0)
         }
     }
 }
